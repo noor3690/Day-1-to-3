6118f62a8693fd4073db4c080507a9e325d775e0
noor3690 <164336345+noor3690@users.noreply.github.com>
2024-03-21 04:56:39 -0600

    Day 1
    100 Days of coding challenge

---
+++ mean.js
@@ -0,0 +1,10 @@
+// Question 2
+var personName = "Noor ul ain";
+console.log("Hellow ".concat(personName, " would you like to learn coding"));
+//Question 3
+var fullname = "Noor ul ain";
+console.log(fullname.toLowerCase());
+console.log(fullname.toUpperCase());
+console.log(fullname.charAt(0).toUpperCase() + fullname.slice(1).toLowerCase());
+var fullna = "Noor ul ain";
+console.log("Hellow ".concat(fullna, " would you like to learn coding"));
---
+++ mean.ts
@@ -0,0 +1,12 @@
+// Question 2
+let personName: string = "Noor ul ain"
+console.log(`Hellow ${personName} would you like to learn coding`)
+
+//Question 3
+let fullname: string = "Noor ul ain";
+console.log(fullname.toLowerCase())
+console.log(fullname.toUpperCase())
+console.log(fullname.charAt(0).toUpperCase() + fullname.slice(1).toLowerCase())
+ 
+
+
