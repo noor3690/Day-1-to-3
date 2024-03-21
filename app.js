88980e0e79d6224a3edb6f667f6b33b42d3efd43
noor3690 <164336345+noor3690@users.noreply.github.com>
2024-03-21 04:58:24 -0600

    Day 3
    100 days of coding challenge

---
+++ app.js
@@ -0,0 +1,24 @@
+// Question 7 and 8
+//Addition
+var a = 4;
+var b = 4;
+console.log("a = 4 and b = 4");
+console.log("a + b: ".concat(a + b));
+//Subtraction
+var c = 12;
+var d = 4;
+console.log("c = 12 and d = 4");
+console.log("c - d: ".concat(c - d));
+//Multiplication
+var e = 4;
+var f = 2;
+console.log("e = 4 and f = 2");
+console.log("e * f: ".concat(e * f));
+// division
+var g = 16;
+var h = 2;
+console.log("g = 16 and h = 2");
+console.log("g / h: ".concat(g / h));
+// question 9
+var favouritenumber = 9;
+console.log("My favourite number is ".concat(favouritenumber));
---
+++ app.ts
@@ -0,0 +1,28 @@
+// Question 7 and 8
+//Addition
+let a: number = 4;
+let b: number = 4;
+console.log("a = 4 and b = 4");
+console.log(`a + b: ${a + b}`);
+
+//Subtraction
+let c: number = 12;
+let d: number = 4;
+console.log("c = 12 and d = 4");
+console.log(`c - d: ${c - d}`);
+
+//Multiplication
+let e: number = 4;
+let f: number = 2;
+console.log("e = 4 and f = 2");
+console.log(`e * f: ${e * f}`);
+
+// division
+let g: number = 16;
+let h: number = 2;
+console.log("g = 16 and h = 2");
+console.log(`g / h: ${g / h}`);
+
+// question 9
+let favouritenumber: number = 9;
+console.log(`My favourite number is ${favouritenumber}`);
