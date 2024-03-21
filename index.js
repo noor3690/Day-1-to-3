79fa67529cc52298d0a73d99fe38ef906b653c70
noor3690 <164336345+noor3690@users.noreply.github.com>
2024-03-21 04:57:22 -0600

    Day 2
    100 days of coding challenge

---
+++ index.js
@@ -0,0 +1,14 @@
+//Question 4
+let Author: string = "XiaoZhan"
+let Quote: string = "Dreams are enough to help you break all your hesitation"
+console.log(`Author: ${Author}\nQuote: ${Quote}`)
+console.log(`${Author} once said, ${Quote}`)
+//Question 5
+let famous_person: string = "XiaoZhan"
+let quote: string = "Dreams are enough to help you break all your hesitation"
+console.log(`${famous_person} once said, ${quote}`)
+//Question 6
+var Name = "\n\t Apiiii \t\n";
+console.log(Name);
+var stripped = Name.trim();
+console.log(stripped);
---
+++ index.ts
@@ -0,0 +1,17 @@
+// Question 4
+let Author: string = "XiaoZhan"
+let Quote: string = "Dreams are enough to help you break all your hesitation"
+console.log(`Author: ${Author}\nQuote: ${Quote}`)
+console.log(`${Author} once said, ${Quote}`)
+
+//Question 5
+let famous_person: string = "XiaoZhan"
+let quote: string = "Dreams are enough to help you break all your hesitation"
+console.log(`${famous_person} once said, ${quote}`)
+
+//Question 6
+let Name: string = "\n\t Apiiii \t\n";
+console.log(Name);
+let stripped = Name.trim();
+console.log(stripped);
+
